135bd2815e5302dcdf992a69a1463096913e07d8
NaitikKaythwal <naitikkaythwal@gmail.com>
2024-04-14 10:22:22 -0500

    Finishing the assignment

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('DOMContentLoaded', function () {
+    const filterButtons = document.querySelectorAll('.filter-button');
+    const items = document.querySelectorAll('.item');
+  
+    filterButtons.forEach(button => {
+      button.addEventListener('click', function () {
+        const filter = button.getAttribute('data-filter');
+        items.forEach(item => {
+          if (filter === 'all' || item.classList.contains(filter)) {
+            item.style.display = 'block';
+          } else {
+            item.style.display = 'none';
+          }
+        });
+      });
+    });
+  });
+  
